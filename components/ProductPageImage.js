41000d0f07dec0200700adf10387996b7b12a29c
muzenjakkevin <muzenjak57@gmail.com>
2021-05-06 20:53:30 +0200

    added alternative images to product page, have not yet fixed to change between images

--- components/ProductPageImage.js
+++ components/ProductPageImage.js
@@ -1,12 +1,15 @@
+import styles from '../styles/ProductPage.module.css'
 import { Image } from 'react-datocms'
 
 const ProductPageImage = ({ data }) => {
 
   console.log(data)
+
   return (
-    <div>
-      {data.product.alternativeImages.map(imageItem => 
-        <Image key={imageItem.responsiveImage.alt} data={imageItem.responsiveImage}/>
+    <div className={styles.alternativeImagesContainer}>
+      {/* <Image data={data.product.mainImage.responsiveImage}/> */}
+      {data.product.alternativeImages.map(imageItem =>
+        <Image style={{width: '50%', justifySelf: 'start',}} key={imageItem.responsiveImage.alt} data={imageItem.responsiveImage}/>
       )}
     </div>
   )
